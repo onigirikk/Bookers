66c669fd8a2cb655c0bdc5466b15948f600b4f5f
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-02-06 14:15:50 +0000

    [Update]3-Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   
+  
   def create
     book = Book.new (book_params)
     book.save
@@ -16,10 +17,24 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
   
   private
   def book_params
-    params.require(:books).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
 
   get '/' => 'homes#top'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show',as:'book'
-  get 'books' => 'books#edit'
-  
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
 end

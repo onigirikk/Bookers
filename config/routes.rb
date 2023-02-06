16c0050b1895a5fe6da565b95da4617bd45b72a2
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-02-06 09:15:50 +0000

    [Update]3-Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
   get '/' => 'homes#top'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get '/books/:id' => 'books#show',as:'book'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show',as:'book'
   get 'books' => 'books#edit'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
